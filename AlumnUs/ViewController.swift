f338edb8e36f89d2223186313a7ef8564642303e
Juhi Nayak <juhinayak07@gmail.com>
2020-03-04 17:31:44 -0800

    Initial Commit

---
+++ AlumnUs/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AlumnUs
+//
+//  Created by Juhi Nayak on 3/4/20.
+//  Copyright © 2020 Juhi Nayak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
